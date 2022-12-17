59b3ec60da09dc4495e43c8abd4f8f3ed5fc9433
Ilya Akkuzin <gr3yknigh1@gmail.com>
2022-12-17 15:59:34 +0300

    Remove old

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include "bst/bst.hpp"
 
 int main() {
-    BST<int, int> bst;
     std::cout << "Hi hi there\n";
     return 0;
 }
--- tests/test_bst.cpp
+++ tests/test_bst.cpp
@@ -1,10 +1,6 @@
 #include <string>
 #include <criterion/criterion.h>
 
-#include "bst/bst.hpp"
-
 Test(test_bst, BST_Init) {
-    BST<int, std::string> bst;
-
     cr_expect(true);
 }
